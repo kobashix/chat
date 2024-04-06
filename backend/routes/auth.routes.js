83fb194438e37d501bdde843a4c1ecfbaab2c108
kobashi <hax@tacos.com>
2024-04-06 03:17:52 -0500

    1st part completed - backend

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,14 @@
+// Authorization Function
+
+import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.get("/signup", signup);
+
+router.get("/login", login);
+
+router.get("/logout", logout);
+
+export default router;
