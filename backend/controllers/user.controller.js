ee4b89609c755f658c7b916c6e375b40421d3d5f
kobashi <hax@tacos.com>
2024-04-06 06:14:43 -0500

    Deployment

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,16 +1,14 @@
 import User from "../models/user.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
-    try {
+	try {
+		const loggedInUserId = req.user._id;
 
-        const loggedInUserId = req.user._id;
+		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
-
-        return res.status(200).json({ users: filteredUsers });
-
-    } catch (error) {
-        console.log("Error in getUsersForSidebar: ", error.message)
-        res.status(500).json({ error: "Internal Server error" });
-    }
-}
+		res.status(200).json(filteredUsers);
+	} catch (error) {
+		console.error("Error in getUsersForSidebar: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -1,24 +1,27 @@
 // Messenger function
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-    senderId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    receiverId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    message:{
-        type: String,
-        required: true
-    }
-    // createdAt, updatedAt
-}, {timestamps: true});
+const messageSchema = new mongoose.Schema(
+	{
+		senderId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		receiverId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		message: {
+			type: String,
+			required: true,
+		},
+		// createdAt, updatedAt
+	},
+	{ timestamps: true }
+);
 
 const Message = mongoose.model("Message", messageSchema);
 
